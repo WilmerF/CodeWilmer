30a4bf088e5bf986fe0584c7d9d74858040332ec
WilmerF <figueroalexis@outlook.es>
2016-02-26 23:08:04 -0500

    Added files via upload

---
+++ Toggle.js
@@ -0,0 +1,18 @@
+
+
+
+
+ $( "span.showhim" ).hover(
+  function() {
+    $( this ).append( $( "div.showme" ) );
+  } 
+);
+
+
+
+	     /*$("div.showme").mouseenter(function(){
+	        $("div.showme").slideDown( "span.showhim");
+	    });
+	    $("div.showme").mouseleave(function(){
+	        $("div.showme").slideUp("span.showhim");
+	    });*/
---
+++ slider.js
@@ -0,0 +1,29 @@
+$(window).load(function() {
+		   // $('#slider').nivoSlider();
+		});
+
+		$('#slider').nivoSlider({
+		    /*effect: 'fade',                 // Specify sets like: 'fold,fade,sliceDown'
+		    slices: 15,                     // For slice animations
+		    boxCols: 8,                     // For box animations
+		    boxRows: 4,                     // For box animations
+		    animSpeed: 500,                 // Slide transition speed
+		    pauseTime: 3000,                 // How long each slide will show
+		    startSlide: 0,                     // Set starting Slide (0 index)
+		    directionNav: true,             // Next & Prev navigation
+		    controlNav: false,                 // 1,2,3... navigation
+		    controlNavThumbs: true,         // Use thumbnails for Control Nav
+		    pauseOnHover: false,             // Stop animation while hovering
+		    manualAdvance: false,             // Force manual transitions*/
+		    prevText: '<img src="file:///D:DRACO/AMADEUS/img/arrow-left.png" class="prevText">',                 // Prev directionNav text
+		    nextText: '<img src="file:///D:DRACO/AMADEUS/img/arrow-right.png" class="nextText">',                 // Next directionNav text
+		    randomStart: false,  
+		    /*randomStart: false,             // Start on a random slide
+		    beforeChange: function(){},     // Triggers before a slide transition
+		    afterChange: function(){},         // Triggers after a slide transition
+		    slideshowEnd: function(){},     // Triggers after all slides have been shown
+		    lastSlide: function(){},         // Triggers when last slide is shown
+		    afterLoad: function(){ 
+		    	//$("#slider .nivo-prevNav, #slider .nivo-nextNav").html('')
+		    	}         // Triggers when slider has loaded*/
+		});
